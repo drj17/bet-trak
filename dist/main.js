c38616d14b22ef061e2f05fce044c7af50d3124a
David Janas <davidjanasr@gmail.com>
2019-11-08 20:12:03 -0500

    yarn -> npm

--- dist/main.js
+++ dist/main.js
@@ -4,9 +4,9 @@ require("dotenv/config");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const port = process.env.PORT || 3000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
+    const port = process.env.PORT || 3000;
     await app.listen(port);
 }
 bootstrap();
