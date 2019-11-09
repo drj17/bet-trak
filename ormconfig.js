5cab080aeab8ba73723f2c93878ecac08c40b85f
David Janas <davidjanasr@gmail.com>
2019-11-08 20:20:15 -0500

    use database_url in ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   type: 'postgres',
-  port: 5432,
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   connectTimeout: 1500,
   logging: true,
